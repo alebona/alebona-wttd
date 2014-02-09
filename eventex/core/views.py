952059cb6af6c0a6f8004546a4892d77cc05942f
alebona <ale.bonna@hotmail.com>
2014-02-09 01:40:10 -0300

    atualizando template

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-	
+def home(request):	
 	return render(request, 'index.html')
+
+def sobre(request):
+	return render(request, 'sobre.html')
+
+def contato(request):
+	return render(request, 'contato.html')
